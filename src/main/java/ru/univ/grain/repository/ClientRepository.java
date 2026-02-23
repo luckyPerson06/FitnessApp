a012b104a5fe96b40f93dc005f309ff26791b113
Aleks <arsenlit212121@gmail.com>
2026-02-23 23:19:00 +0300

    refactor: remove unused legacy code

--- src/main/java/ru/univ/grain/repository/ClientRepository.java
+++ src/main/java/ru/univ/grain/repository/ClientRepository.java
@@ -1,44 +1,3 @@
-/*
-package ru.univ.grain.repository;
-
-import org.springframework.stereotype.Repository;
-import ru.univ.grain.domain.Client;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository
-public class ClientRepository {
-    private final List<Client> storage = new ArrayList<>();
-    private long idSequence = 0;
-
-    public Client save(Client client) {
-        client.setId(idSequence++);
-        storage.add(client);
-        return client;
-    }
-
-    public Client findById(long id) {
-        return storage.stream()
-                .filter(client -> client.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Client not found"));
-    }
-
-
-    public List<Client> findByLastName(String lastName) {
-        return storage.stream()
-                .filter(client -> client.getLastName().equalsIgnoreCase(lastName))
-                .toList();
-    }
-
-    public void deleteById(long id) {
-        storage.removeIf(client -> client.getId() == id);
-    }
-
-
-}*/
-
 package ru.univ.grain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -52,6 +11,5 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
 
     Client findFirstById(long id);
     List<Client> findByLastNameIgnoreCase(String lastName);
-
 
 }
