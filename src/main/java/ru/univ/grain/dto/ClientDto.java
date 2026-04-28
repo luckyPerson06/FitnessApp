5c5c2e2ba1f5ea94d17537f60f7e3c88c670b704
Aleks <arsenlit212121@gmail.com>
2026-04-28 09:33:20 +0300

    refactor: extract common client fields to base class

--- src/main/java/ru/univ/grain/dto/ClientDto.java
+++ src/main/java/ru/univ/grain/dto/ClientDto.java
@@ -1,7 +1,6 @@
 package ru.univ.grain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -10,26 +9,5 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Schema(description = "Запрос на создание клиента")
-public class ClientDto {
-
-    @Schema(description = "Имя клиента", example = "Иван", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "Имя обязательно")
-    private String firstName;
-
-    @Schema(description = "Отчество клиента", example = "Иванович")
-    private String middleName;
-
-    @Schema(description = "Фамилия клиента", example = "Иванов", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "Фамилия обязательна")
-    private String lastName;
-
-    @Schema(description = "Номер телефона", example = "+375291234567", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "Телефон обязателен")
-    @Pattern(regexp = "^\\+?\\d{10,15}$", message = "Неверный формат телефона")
-    private String phoneNumber;
-
-    @Schema(description = "Email клиента", example = "ivan@mail.com", requiredMode = Schema.RequiredMode.REQUIRED)
-    @Email(message = "Неверный формат email")
-    @NotBlank(message = "Email обязателен")
-    private String email;
+public class ClientDto extends BaseClientRequest {
 }
--- src/main/java/ru/univ/grain/dto/RegisterRequest.java
+++ src/main/java/ru/univ/grain/dto/RegisterRequest.java
@@ -1,9 +1,7 @@
 package ru.univ.grain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -12,28 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Schema(description = "Запрос на регистрацию нового клиента")
-public class RegisterRequest {
-
-    @Schema(description = "Имя клиента", example = "Иван", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "Имя обязательно")
-    private String firstName;
-
-    @Schema(description = "Отчество клиента", example = "Иванович")
-    private String middleName;
-
-    @Schema(description = "Фамилия клиента", example = "Иванов", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "Фамилия обязательна")
-    private String lastName;
-
-    @Schema(description = "Номер телефона", example = "+375291234567", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NotBlank(message = "Телефон обязателен")
-    @Pattern(regexp = "^\\+?\\d{10,15}$", message = "Неверный формат телефона")
-    private String phoneNumber;
-
-    @Schema(description = "Email клиента", example = "ivan@mail.com", requiredMode = Schema.RequiredMode.REQUIRED)
-    @Email(message = "Неверный формат email")
-    @NotBlank(message = "Email обязателен")
-    private String email;
+public class RegisterRequest extends BaseClientRequest {
 
     @Schema(description = "Пароль", example = "password123", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "Пароль обязателен")
