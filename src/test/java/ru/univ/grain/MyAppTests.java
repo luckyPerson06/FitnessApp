48b603ef59043c84c356d9a55269697d510152a4
Aleks <arsenlit212121@gmail.com>
2026-05-06 20:15:16 +0300

    ci: update CI/CD pipeline structure

--- src/test/java/ru/univ/grain/MyAppTests.java
+++ src/test/java/ru/univ/grain/MyAppTests.java
@@ -2,12 +2,12 @@ package ru.univ.grain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyAppTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
